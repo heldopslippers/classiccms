cafc99605365489d6e35a33dc53ba8bc30f43d31
jeljer te Wies <heldopslippers@gmail.com>
2012-06-12 10:56:57 +0200

    added content_type to css and js

--- lib/classiccms/controllers/website.rb
+++ lib/classiccms/controllers/website.rb
@@ -16,10 +16,12 @@ module Classiccms
     end
     get '/javascripts/*.js' do
       response.headers['Cache-Control'] = ENV['RACK_ENV'] == 'development' ? 'no-cache' : 'public, max-age=86400'
+      content_type :js
       show params[:splat].join, :views => [File.join(Dir.pwd, 'public/javascripts')]
     end
     get '/stylesheets/*.css' do
       response.headers['Cache-Control'] = ENV['RACK_ENV'] == 'development' ? 'no-cache' : 'public, max-age=86400'
+      content_type :css
       show params[:splat].join, :views => [File.join(Dir.pwd, 'public/stylesheets')]
     end
     get '/:id/?*' do
