013410550611d761e679260259cefcdd367026a9
heldopslippers <heldopslippers@gmail.com>
2012-09-17 21:27:43 +0200

    fixed problem

--- lib/classiccms/models/image.rb
+++ lib/classiccms/models/image.rb
@@ -4,6 +4,6 @@ class Image
   field :file_uid
   field :file_name
 
-  image_accessor :file
+  file_accessor :file
   validates_property :format, :of => :file, :in => [:jpg, :jpeg, :png, :gif]
 end
--- lib/classiccms/version.rb
+++ lib/classiccms/version.rb
@@ -1,3 +1,3 @@
 module Classiccms #:nodoc
-  VERSION = "0.5.16"
+  VERSION = "0.5.17"
 end
