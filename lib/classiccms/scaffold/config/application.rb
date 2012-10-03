3b2a7d08a78ace2868e66e0d42ab08d746479f59
heldopslippers <heldopslippers@gmail.com>
2012-10-03 15:14:06 +0200

    fixed another css bug in CKeditor and bumped version of the gem to 0.6.0

--- lib/classiccms/scaffold/config/application.rb
+++ lib/classiccms/scaffold/config/application.rb
@@ -1,3 +1,6 @@
 module Classiccms
   set :session_secret, 'a6d49bfcafc6ac1cae95c0b55c1069f6cc57b7866b6a6b7c1ed23dd09ad4c8c1'
+  $app = Dragonfly[:file].configure_with(:imagemagick)
+  $app.define_macro_on_include(Mongoid::Document, :file_accessor)  
+  $app.server.url_format = '/files/:job/:basename.:format'
 end
